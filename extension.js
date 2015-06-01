80da4fc6def2b9571a3afdfb2a241b7aebc5afd8
kevmatjad2000 <lukykunc1@seznam.cz>
2015-06-01 19:04:41 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -55,13 +55,13 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-        botName: "basicBot",
-        language: "english",
+        botName: "#ℳℭƧBot",
+        language: "czech",
         startupCap: 1, // 1-200
         startupVolume: 0, // 0-100
         startupEmoji: false, // true or false
         cmdDeletion: true,
-        chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
+        chatLink: "https://cdn.rawgit.com/Yemasthui/basicBot/ab0c31bc4ea71d25b394bcd3a631c5d75c15ccf5/lang/en.json",
         maximumAfk: 120,
         afkRemoval: true,
         maximumDc: 60,
@@ -93,7 +93,7 @@
         afkRankCheck: "ambassador",
         motdEnabled: false,
         motdInterval: 5,
-        motd: "Temporary Message of the Day",
+        motd: "",
         filterChat: true,
         etaRestriction: false,
         welcome: true,
